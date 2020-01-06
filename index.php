c8694eb4f29909c5e6d50cad9d24774f742e2cc0
Simon <watsonsemon@gmail.com>
2020-01-06 15:10:36 +1300

    Loops through server fodler and gets each file ready for Network requests

--- index.php
+++ index.php
@@ -4,23 +4,36 @@
 		<meta charset="utf-8">
 		<title></title>
 		<script>
-			<?php 
-			
+			<?php
+
+			// Goes through the serer folder and adds each file as a Network request for AJAX
+			$serverFiles = scandir("./private/server/");
+			$requestObj = "{";
+			foreach ($serverFiles as $serverFile) {
+				if ($serverFile[0] == ".") continue;
+				else if (strpos($serverFile, ".php")) {
+					$fileName = substr($serverFile, 0, strrpos($serverFile, "."));
+					$requestObj .= "$fileName: \"$fileName\",";
+				}
+			}
+			$requestObj .= "};\n\n";
+			echo "const Network = $requestObj";
+
 			function recurseIncludeJS($path) {
 				$js = scandir($path);
 				foreach ($js as $jsFile) {
 					if ($jsFile[0] == ".") continue;
 					else if (is_dir($path . $jsFile)) recurseIncludeJS($path . $jsFile . "/");
 					else echo file_get_contents($path . $jsFile) . "\n\n";
-				}	
+				}
 			}
-			
+
 			// Include the JavaScript for every file in private/client/``
 			recurseIncludeJS("./private/client/");
 			?>
 		</script>
 	</head>
 	<body>
-		
+
 	</body>
 </html>
