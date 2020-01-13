093924b8becf0533119a7afc86f7e0f5aa869acf
Orlando Parisblue <oparisblue@gmail.com>
2020-01-13 15:12:25 +1300

    WIP 2

--- private/client/constants.js
+++ private/client/constants.js
@@ -6,7 +6,7 @@
 const Constants = {
 	DARK: {
 		BACKGROUND: [
-			"", // Darkest
+			"#", // Darkest
 			"",
 			"",
 			"",
@@ -29,3 +29,15 @@ const Constants = {
 	UI_SIZE: 1, // 0.5 = small, 1 = normal, 2 = large
 	FONT_FAMILY: "Trebuchet MS",
 };
+
+function getColour(value) {
+	return Constants[window.matchMedia("(prefers-color-scheme: dark)") ? "DARK" : "LIGHT"].BACKGROUND[value];
+}
+
+function getAccentColour() {
+	
+}
+
+function getAccentHighlight() {
+	
+}
