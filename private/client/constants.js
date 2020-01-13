c907eed731be2c71561c2fc1d1592889d21dcbf7
Simon <watsonsemon@gmail.com>
2020-01-13 15:10:29 +1300

    Added default values

--- private/client/constants.js
+++ private/client/constants.js
@@ -6,25 +6,25 @@
 const Constants = {
 	DARK: {
 		BACKGROUND: [
-			"", // Darkest
-			"",
-			"",
-			"",
-			"", // Lightest
+			"#212121", // Darkest
+			"#424242",
+			"#616161",
+			"#757575",
+			"#9E9E9E", // Lightest
 		],
-		TEXT: "",
+		TEXT: "#FFFFFF",
 	},
 	LIGHT: {
 		BACKGROUND: [
-			"", // Darkest
-			"",
-			"",
-			"",
-			"", // Lightest
+			"#FFFFFF", // Darkest (White)
+			"#FAFAFA",
+			"#F5F5F5",
+			"#EEEEEE",
+			"#E0E0E0", // Lightest (Darker White)
 		],
-		TEXT: "",
+		TEXT: "#000000",
 	},
-	BORDER_RADIUS: "",
+	BORDER_RADIUS: "8px",
 	REDUCE_MOTION: false,
 	UI_SIZE: 1, // 0.5 = small, 1 = normal, 2 = large
 	FONT_FAMILY: "Trebuchet MS",
