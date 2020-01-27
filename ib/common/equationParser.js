50a60ab06c70808df993363f7e96ed4d8795c13b
Simon <watsonsemon@gmail.com>
2020-01-27 18:09:13 +1300

    Added tokenList parser into EquationParser

--- ib/common/equationParser.js
+++ ib/common/equationParser.js
@@ -8,6 +8,44 @@ class EquationParser {
 	
 	// String => List of Tokens
 	makeTokenList() {
+		let typeState = "";
+		let funcCounter = 0;
+		let tokens = ["+", "-", "*", "/", "%", "^", "(", ")"];
+		let funcs = ["abs(", "sin(", "cos(", "tan(", "sqrt(", "ceil(", "floor(", "round("];
+		// Loop through the equation string
+		for (let i = 0; i < this.equation.length; i++) {
+			let char = this.equation[i];
+			// if the character is empty space, continue
+			if (char == " ") continue;
+			// If the current character is a continuation of a function name, add the prevoius characters to
+			// the current letter in reverce order
+			if (typeState == "func" && funcCounter != 0) {
+				char = "";
+				for (let j = i - funcCounter; j <= i; j++) char += this.equation[j];
+			}
+			// If the character is a number,
+			if (!isNaN(char)) {
+				// If the character is a continuation of a previous number, add it to it. Otherwise push to array
+				if (typeState == "num") this.tokenList[this.tokenList.length] += char;
+				else this.tokenList.push(char);
+				typeState = "num";
+			// If the character is a token, add it to the array
+			} else if (tokens.includes(char)) {
+				this.tokenList.push(char);
+				typeState = "opp";
+			// If the character is apart of a function, 
+			} else if (funcs.filter((func) => func.indexOf(char) == 0).length > 0) {
+				typeState = "func";
+				let finalFunc = false;
+				// Check if it is exactly the same as any of the function names
+				for (let j = 0; j < funcs.length; j++) if (funcs[j] == char) finalFunc = true;
+				// if it is, add the whole thing to the list
+				if (finalFunc) {
+					this.tokenList.push(char);
+					funcCounter = 0;
+				} else funcCounter++;
+			}
+		}
 		return this;
 	}
 	
@@ -41,3 +79,12 @@ class EquationParser {
 	}
 	
 }
+
+function testHelper() {
+	let inst = new EquationParser("3 + 1 / sin(6 ^ 7)").makeTokenList();
+	
+	console.log(inst.tokenList);
+	
+}
+
+testHelper();
